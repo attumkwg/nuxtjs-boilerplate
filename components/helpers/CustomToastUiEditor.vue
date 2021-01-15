4daba3e1278349a06f431230989b05ecd1eaf3eb
髙濱敦弘 <takahama1992@gmail.com>
2021-01-15 16:37:47 +0900

    add toast ui editor (if you dont need it, remove)

---
+++ components/helpers/CustomToastUiEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <client-only>
+    <editor v-model="editorText" />
+  </client-only>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import 'tui-editor/dist/tui-editor.css'
+import 'tui-editor/dist/tui-editor-contents.css'
+import 'codemirror/lib/codemirror.css'
+
+export default Vue.extend({
+  components: {
+    Editor: () => {
+      if (process.client) {
+        return import ('@toast-ui/vue-editor/src/Editor.vue')
+      }
+    }
+  },
+  data() {
+    return {
+      editorText: ''
+    }
+  }
+})
+</script>
